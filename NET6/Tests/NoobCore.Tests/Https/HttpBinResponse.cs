d47b440b3c0364e23d6964499f88c68cac4fee5c
noob <noobwu@hotmail.com>
2022-04-01 22:36:22 +0800

    feat(UnitTests:Http): add http unit test
    feat(UnitTests:Http): add http unit test

---
+++ NET6/Tests/NoobCore.Tests/Https/HttpBinResponse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace NoobCore.Tests.Https
+{
+    [DataContract]
+    public class HttpBinResponse
+    {        
+        [DataMember(Name = "args")]
+        public Dictionary<string, string> Args { get; set; }
+
+        [DataMember(Name = "data")]
+        public string Data { get; set; }
+
+        [DataMember(Name = "files")]
+        public Dictionary<string, string> Files { get; set; }
+
+        [DataMember(Name = "form")]
+        public Dictionary<string, string> Form { get; set; }
+
+        [DataMember(Name = "headers")]
+        public Dictionary<string, string> Headers { get; set; }
+
+        [DataMember(Name = "json")]
+        public object JSON { get; set; }
+
+        [DataMember(Name = "origin")]
+        public string Origin { get; set; }
+
+
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+    }
+}
---
+++ NET6/Tests/NoobCore.Tests/Https/HttpTests.cs
@@ -0,0 +1,64 @@
+﻿// ***********************************************************************
+// Assembly         : NoobCore.Tests
+// Author           : Administrator
+// Created          : 04-01-2022
+//
+// Last Modified By : Administrator
+// Last Modified On : 04-01-2022
+// ***********************************************************************
+// <copyright file="HttpTests.cs" company="NoobCore.Tests">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Utils;
+using System.Net;
+
+namespace NoobCore.Tests.Https
+{
+    /// <summary>
+    /// Defines test class HttpTests.
+    /// </summary>
+    [TestFixture]
+    public class HttpTests
+    {
+        /// <summary>
+        /// Defines the test method BasicCreation.
+        /// </summary>
+        [TestCase]
+        public async Task BasicCreation()
+        {
+            var request = Http.Request("https://example.com/")
+                              .SendPlaintext("test")
+                              .ExpectString();
+            Assert.AreEqual("https://example.com/", request.Inner.Message.RequestUri.ToString());
+
+            var content = request.Inner.Message.Content;
+            Assert.IsInstanceOf<StringContent>(content);
+            var stringContent = await content.ReadAsStringAsync();
+            Assert.AreEqual("test", stringContent);
+        }
+
+        /// <summary>
+        /// Defines the test method BasicDelete.
+        /// </summary>
+        [TestCase]
+        public async Task BasicDelete()
+        {
+            var result = await Http.Request("https://httpbin.org/delete")
+                                    .ExpectJson<HttpBinResponse>()
+                                    .DeleteAsync();
+            Assert.True(result.Success);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual("https://httpbin.org/delete", result.Data.Url);
+            Assert.AreEqual(Http.DefaultSettings.UserAgent, result.Data.Headers["User-Agent"]);
+        }
+
+    }
+}
